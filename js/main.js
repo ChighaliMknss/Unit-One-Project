8f1d39a88b807f384b8f9d3e67886d4bf318541e
Chighali Mouknass <chighalimouknass@Chighalis-MacBook-Pro.local>
2019-08-09 09:32:49 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,56 +1,118 @@
-    //defining my variables
-    let randomWord;
-    let wordGuessed;
-    letters= [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','z','z'];
-    lives = 0;
-    words = ["cat", "dog", "cow", "reindeer","green", "blue","yellow", "turquoise","pink","white","red","january", "february","march","april","May","June","july","August","september", "october", "november", "december","titanic", "forestGump","troll2","mule",
-    "united states","Madagascar","morroco","brazil","malta","nicaragua","mauritius","bulgaria","venzuela","colombia","mauritania","congo","tunisia"]
-    
-    //display messages
-    messages = {
-        win: 'You win!',
-        lose: 'Game over!',
-        guessed: ' already guessed, please try again...',
-        validLetter: 'Please enter a letter from A-Z'
-    };
-   
-    //Word random selection 
-    randomWord = words[Math.floor(Math.random() * words.length)];
-    
-    //break word into comparable characters
-    let currentWord = randomWord.split("");
+// defining my variables
+let randomWord;
+let lives;
+let wordGuessed = [];
+let correctGuess;
+let displayVar; 
+let guess;
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+     's','t', 'u', 'v', 'w','x', 'y', 'z', 'z', 'z'];
+words = ["cat", "dog", "cow", "reindeer", "green", "blue", "yellow", "turquoise", "pink", "white", "red", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "titanic", "forestGump", "troll", "mule",
+    "united states", "madagascar", "morroco", "brazil", "malta", "nicaragua", "mauritius", "bulgaria", "venzuela", "colombia", "mauritania", "congo", "tunisia"]
 
-    console.log(currentWord);
+//Element references 
+let output = document.getElementById("string");
+let count = document.getElementById("lives");
+let buttons= document.querySelectorAll(".btn");
+let playBtn = document.getElementById("playBtn");
 
-    wordGuessed = new Array(currentWord.length);
+/*----------------------- Functions --------------------------*/
+randomWord = words[Math.floor(Math.random() * words.length)];
+
+//break word into comparable characters
+let currentWord = randomWord.split("");
+
+console.log(currentWord);
+
+//display lives function
+
+// let outputFx = function () {
+//     count.innerHTML = "You have " + lives + " lives";
+//     if (lives < 1) {
+//       count.innerHTML = "Game Over";
+//     }
+//     for (var i = 0; i < geusses.length; i++) {
+//       if (counter === geusses[i]) {
+//         count.innerHTML = "You Win!";
+//       }
+//     }
+//   }
 
 
- 
 
-    //function display empty letter spaces
-    document.getElementById("run-btn").addEventListener('click', function display(){
-        currentWord.forEach(function(){
-        document.getElementById("string").innerHTML += "_ ";
-        
-    })
-    })
-    
-    //get a letter 
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach(function (button, idx){
+
+
+// make the array holding the guessed letters equal to the array of the random word generated
+wordGuessed = new Array(currentWord.length);
+
+//function display empty letter spaces
+playBtn.addEventListener('click', function display() {
+    // show underscores
+        displayVar = new Array(currentWord.length)
+        displayVar.fill('_');
+        console.log(displayVar)
+    //     displayVar.forEach(function() {
+    //     stringEl.innerHTML += "_ ";
+    // })
+})
+// conditions 
+if (lives <=0 ) {
+
+    if (wordGuessed !== currentWord){
+        //
+        buttons.forEach(function (button, idx){
         button.setAttribute('value',letters[idx-1])
         button.addEventListener('click', function(evt){
             
             currentWord.forEach(function(el,idx){
-                if (el === evt.target.value)
+                if (el === evt.target.value) 
                 /*for splice
                 * @param first starting index
                 * @param second how many things to remove
                 * items put instead of removed items 
                 */
-                    return wordGuessed.splice(idx,1,evt.target.value);
-                    
+               return wordGuessed.splice(idx,1,evt.target.value);
+                   
             })
-                console.log(wordGuessed);
+                
         })
-    })
+    }) 
+    //
+
+    //get a letter 
+    
+
+    // buttons.forEach(function (button, idx) {
+    //     button.setAttribute('value', letters[idx - 1])
+    //     button.addEventListener('click', function(evt) {
+    //         currentWord.forEach(function (el, idx) {
+    //             if (el === evt.target.value) {
+    //                 wordGuessed.splice(idx, 0, evt.target.value);
+    //                 // displayVar.splice(idx, 1, evt.target.value);
+    //                 output.innerHTML += displayVar[idx];
+    //                 lives--;
+    //             }
+    //             else lives++;
+    //         })
+            
+    //         console.log(wordGuessed);
+            
+            
+    //     })
+    //     console.log(lives);
+    // })
+    console.log(wordGuessed);
+}
+
+else if (wordGuessed.length === currentWord.length){
+
+    wordGuessed.join("");
+    if ( wordGuessed === currentWord){
+
+    output.innerHTML = " You Won !";
+    }
+}
+else output.innerHTML = " Try Again ";
+}
+
