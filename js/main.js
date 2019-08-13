8d2a1938d694ae89914aa6dd86dfbe1909956f0d
Chighali Mouknass <chighalimouknass@Chighalis-MacBook-Pro.local>
2019-08-13 15:46:43 -0500

    CSS update

--- js/main.js
+++ js/main.js
@@ -1,56 +1,85 @@
-    //defining my variables
-    let randomWord;
-    let wordGuessed;
-    letters= [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','z','z'];
-    lives = 0;
-    words = ["cat", "dog", "cow", "reindeer","green", "blue","yellow", "turquoise","pink","white","red","january", "february","march","april","May","June","july","August","september", "october", "november", "december","titanic", "forestGump","troll2","mule",
-    "united states","Madagascar","morroco","brazil","malta","nicaragua","mauritius","bulgaria","venzuela","colombia","mauritania","congo","tunisia"]
-    
-    //display messages
-    messages = {
-        win: 'You win!',
-        lose: 'Game over!',
-        guessed: ' already guessed, please try again...',
-        validLetter: 'Please enter a letter from A-Z'
-    };
-   
-    //Word random selection 
+let randomWord;
+let lives;
+let wordGuessed = [];
+let currentWord;
+let guess;
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'z', 'z'];
+words = ["cat", "dog", "cow", "bear", "green", "blue", "yellow", "turquoise", "pink", "white", "red", "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "titanic", "forestGump", "troll", "mule",
+    "united states", "madagascar", "morroco", "brazil", "malta", "nicaragua", "germany", "bulgaria", "venzuela", "colombia", "spain", "australia", "france",]
+
+//Element references 
+let output = document.getElementById("string");
+let count = document.getElementById("lives");
+let hangmanImg = document.querySelector("hangman");
+let buttons = document.querySelectorAll(".btn");
+let playBtn = document.getElementById("playBtn");
+
+/*------------Events Listeners---------------*/
+displayFx();
+
+/*---------------------Functions---------------*/
+
+
+function displayFx() {
+
+    document.querySelector(".letter-btn").style.display = 'inline-flex';
     randomWord = words[Math.floor(Math.random() * words.length)];
-    
-    //break word into comparable characters
-    let currentWord = randomWord.split("");
-
+    currentWord = randomWord.split("");
     console.log(currentWord);
-
     wordGuessed = new Array(currentWord.length);
+    wordGuessed.fill("_ ");
+    lives = 8;
+    render(wordGuessed);
+}
 
-
- 
-
-    //function display empty letter spaces
-    document.getElementById("run-btn").addEventListener('click', function display(){
-        currentWord.forEach(function(){
-        document.getElementById("string").innerHTML += "_ ";
-        
-    })
-    })
-    
-    //get a letter 
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach(function (button, idx){
-        button.setAttribute('value',letters[idx-1])
-        button.addEventListener('click', function(evt){
-            
-            currentWord.forEach(function(el,idx){
-                if (el === evt.target.value)
-                /*for splice
-                * @param first starting index
-                * @param second how many things to remove
-                * items put instead of removed items 
-                */
-                    return wordGuessed.splice(idx,1,evt.target.value);
-                    
+buttons.forEach(function (button, idx) {
+    button.setAttribute('value', letters[idx])
+    button.addEventListener('click', function (evt) {
+        if (currentWord.find(x => x === evt.target.value)) {
+            currentWord.forEach(function (el, idx) {
+                if (el === evt.target.value) {
+                    wordGuessed.splice(idx, 1, evt.target.value);
+                }
             })
-                console.log(wordGuessed);
-        })
-    })
+        } else {
+
+            lives--;
+        }
+        evt.target.style.visibility = 'hidden';
+        render(wordGuessed);
+    });
+})
+
+
+
+
+function render(wordGuessed) {
+
+    if (lives <= 0){
+        alert("TRY AGAIN");
+        window.location.reload();
+    }
+    else if (wordGuessed.join('') === currentWord.join('')){
+        winFx();
+        output.style.display = 'none';
+        playBtn.style.opacity = 1;
+        playBtn.addEventListener('click', displayFx);
+    
+
+    }
+    else {
+        count.textContent = `Lives : ${lives}`;
+    }
+    output.textContent = "";
+    currentWord.forEach(function (x, idx) {
+        output.textContent += wordGuessed[idx];
+    });
+}
+
+
+function winFx() {
+    alert("(^_^) YOU GUESSED IT !");
+    window.location.reload();
+}
